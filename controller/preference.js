34c8c5eb6da83589666c1d00a02224fff5d64c42
abdullah2891 <abdullah2891@gmail.com>
2016-05-16 05:33:37 +0600

    can add preference

---
+++ controller/preference.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var User = require('../model/user');
+
+
+exports.isGoing = function(req,res){
+  var user = req.session.passport.user;
+
+  var newUser = new User();
+
+  newUser.name = user.displayName;
+  newUser.id = user.id;
+  newUser.location = req.body.location;
+
+  newUser.save(function(err,user){
+    if(err){
+      res.send("error");
+    }else{
+      console.log(user);
+      res.json(user);
+    }
+  });
+
+}
